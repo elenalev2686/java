47ff1852f0774365756eaa70d17a01c2f7dbc7dc
elenalev2686 <elenalev2686@gmail.com>
2023-07-28 17:40:19 +0700

    rechanged src

--- src/Main.java
+++ src/Main.java
@@ -16,8 +16,5 @@ public class Main {
                 System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
                 System.out.println((spices / eaters) + " ложек(ки/ка) специй");
 
-            }
-        }
-        }
     }
 }
